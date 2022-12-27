4488d516cb860e5971f971bf1b9cf1c8e2ed9171
Deandra Falih <deandrafalih@student.uns.ac.id>
2022-12-27 21:39:53 +0700

    First_Commit

---
+++ backend/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/siswa', [SiswaController::class, 'index']);
+Route::get('/siswa/{id}', [SiswaController::class, 'show']);
+Route::post('/siswa', [SiswaController::class, 'store']);
+Route::put('/siswa/{id}', [SiswaController::class, 'update']);
+Route::delete('/siswa/{id}', [SiswaController::class, 'destroy']);
+ 
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
